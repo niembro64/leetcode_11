3d1a5c32ae8bb771832caccaff29bc9d6b97280e
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-08-03 00:00:32 -0400

    init

---
+++ leetcode_11.js
@@ -0,0 +1,10 @@
+function maxArea(height) {
+    return 0;
+}
+var x = [
+    [1, 8, 6, 2, 5, 4, 8, 3, 7],
+    [1, 1],
+];
+x.forEach(function (x, i) {
+    console.log(x);
+});
---
+++ leetcode_11.ts
@@ -0,0 +1,12 @@
+function maxArea(height: number[]): number {
+    return 0;
+}
+
+let x: number[][] = [
+    [1, 8, 6, 2, 5, 4, 8, 3, 7],
+    [1, 1],
+];
+
+x.forEach((x, i) => {
+    console.log(x);
+});
