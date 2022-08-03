75cc49fd90b3fb10d27f4c51d4f9e27264f32aaf
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-08-03 00:45:00 -0400

    ehhh

--- leetcode_11.js
+++ leetcode_11.js
@@ -1,10 +1,40 @@
 function maxArea(height) {
-    return 0;
+    if (height.length < 2) {
+        return 0;
+    }
+    if (height.length === 2) {
+        if (height[0] < height[1]) {
+            return height[0];
+        }
+        else {
+            return height[1];
+        }
+    }
+    var largest = { left: -1, right: -1, volume: -1 };
+    var curr = -1;
+    for (var i = 0; i < height.length - 1; i++) {
+        for (var j = i + 1; j < height.length; j++) {
+            curr = (j - i) * (height[i] < height[j] ? height[i] : height[j]);
+            if (curr > largest.volume) {
+                largest.volume = curr;
+                largest.left = i;
+                largest.right = j;
+            }
+        }
+    }
+    return largest.volume;
 }
 var x = [
     [1, 8, 6, 2, 5, 4, 8, 3, 7],
     [1, 1],
+    [3],
+    [],
+    [2, 2, 1],
+    [3, 1, 3],
+    [2, 1],
+    [1, 2],
 ];
 x.forEach(function (x, i) {
-    console.log(x);
+    console.log(x, maxArea(x));
+    console.log("_____________");
 });
